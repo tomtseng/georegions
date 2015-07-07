d22fb8572f379c4be602dfe0d1d73927e803022d
Tom <ttseng@avvo.com>
2015-07-07 14:58:46 -0700

    Fix read script

--- script/read.rb
+++ script/read.rb
@@ -2,7 +2,7 @@ require 'rgeo-shapefile'
 
 path = (Rails.root + 'shapefile/cb_2014_us_zcta510_500k.shp').to_s
 
-RGeo::Shapefile::Reader.open(path, :factory => FACTORY) do |file|
+RGeo::Shapefile::Reader.open(path, :factory => Zcta::FACTORY) do |file|
   file.each do |record|
     zcta = record['ZCTA5CE10'].to_i
     Zcta.create(:zcta => zcta, :region => record.geometry.projection)
