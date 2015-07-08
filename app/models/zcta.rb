fb0117c51016e20a965f92929ef30e03cc30b063
Tom <ttseng@avvo.com>
2015-07-08 14:19:46 -0700

    Temporary fix of scaling issues
    Turns out my units of measure are not meters, but rather the map units of Web
    Mercator. This is off by a factor of cos(latitude). For now, I am fixing this
    problem by adjusting by the scale factor. This is fine for small distances but
    is bad for long distances where the latitude changes significantly. It's also
    bad since straight lines on the Mercator projection are NOT straight lines on
    the globe (and vice versa).

--- app/models/zcta.rb
+++ app/models/zcta.rb
@@ -7,14 +7,21 @@ class Zcta < ActiveRecord::Base
     :emit_ewkb_srid => true, :hex_format => true)
 
   def self.near_latlon(lat, lon, radius)
+    scale = proj_to_meters_scale_factor(lat)
+    radius_in_m = radius / scale
     point = FACTORY.point(lon, lat).projection
-    nearby_raw = where("ST_Intersects(region, ST_Buffer(ST_GeomFromWKB('#{point.as_binary}', #{point.srid}), #{radius}))")
+    nearby_raw = where("ST_Intersects(region, ST_Buffer(ST_GeomFromWKB('#{point.as_binary}', #{point.srid}), #{radius_in_m}))")
     nearby = nearby_raw.map { |zcta|
       {
         :zcta => zcta.zcta,
-        :distance => point.distance(FACTORY.projection_factory.multi_polygon(zcta.region))
+        :distance => point.distance(FACTORY.projection_factory.multi_polygon(zcta.region)) * scale
       }
     }
     nearby.sort_by { |k| k[:distance] }
+  end
+
+  private
+  def self.proj_to_meters_scale_factor(lat)
+    Math::cos(lat * Math::PI / 180)
   end
 end
